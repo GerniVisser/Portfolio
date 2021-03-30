abed9a67c6385e9e10907178204f4efb1494869e
Gerni Visser <46093495+Zore69@users.noreply.github.com>
2021-03-30 16:49:39 +0200

    feat(WIP) - Github Api integration

--- ProtfolioBackend/Controllers/HomeController.cs
+++ ProtfolioBackend/Controllers/HomeController.cs
@@ -2,32 +2,49 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ProtfolioBackend.Models;
+using ProtfolioBackend.Models.dto;
 
 namespace ProtfolioBackend.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _clientFactory;
 
-        public HomeController(ILogger<HomeController> logger)
+        dtoGithubReadMe dtoReadMe;
+
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory clientFactory)
         {
             _logger = logger;
+            _clientFactory = clientFactory;
         }
 
-        public IActionResult Index()
+        public async Task getReadMe()
         {
-            return View();
+            var request = new HttpRequestMessage(HttpMethod.Get,
+            "https://api.github.com/repos/Sentdex/GameGAN_code/readme");
+            request.Headers.Add("Accept", "application/vnd.github.v3+json");
+            request.Headers.Add("User-Agent", "HttpClientFactory-Sample");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                dtoReadMe = await response.Content.ReadFromJsonAsync<dtoGithubReadMe>();
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
         }
 
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
---
+++ ProtfolioBackend/Models/dto/dtoGithubReadMe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProtfolioBackend.Models.dto
+{
+    public class dtoGithubReadMe
+    {
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public string Url { get; set; }
+        public string Content { get; set; }
+        public string Encoding { get; set; }
+    }
+
+}
