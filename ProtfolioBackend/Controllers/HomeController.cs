235e6f25b0e81ceeca5f337765f4607a3c88862e
Gerni Visser <46093495+Zore69@users.noreply.github.com>
2021-04-13 20:12:03 +0200

    test - Added a test controlle to test DB connection

--- ProtfolioBackend/Controllers/HomeController.cs
+++ ProtfolioBackend/Controllers/HomeController.cs
@@ -6,24 +6,27 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using ProtfolioBackend.BusinessLogic.Processes.Github;
 using ProtfolioBackend.Models.data;
+using ProtfolioBackend.Models.Data;
 
 namespace ProtfolioBackend.Controllers
 {
     [ApiController]
-    [Route("api/[]")]
+    [Route("api/[controller]")]
     public class HomeController : ControllerBase
     {
         private IGitHub _github;
-
+        private readonly DataContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger, IGitHub github)
+        public HomeController(ILogger<HomeController> logger, IGitHub github, DataContext context)
         {
             _logger = logger;
             _github = github;
+            _context = context;
         }
 
         [Route("Get")]
@@ -31,6 +34,14 @@ namespace ProtfolioBackend.Controllers
         public async Task<ActionResult<dtoGithubReadMe>> Get()
         {
             dtoGithubReadMe result = await _github.getReadMe();
+            return result;
+        }
+
+        [Route("Getdb")]
+        [HttpGet]
+        public async Task<ActionResult<dtoGithubReadMe>> Getdb()
+        {
+            var result = await _context.Github.FirstOrDefaultAsync();
             return result;
         }
 
